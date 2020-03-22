4fd25aff3287b23492730553afa87b349c7fbba3
Rina <sutan.gnst@gmail.com>
2020-03-22 08:44:23 +0700

    fix manifest problem

--- src/utils/metaGenerator.tsx
+++ src/utils/metaGenerator.tsx
@@ -4,11 +4,6 @@ interface Meta {
     hid?: string
     content?: string
     name?: string
-    property?: string
-    rel?: string
-    type?: string
-    href?: string
-    sizes?: string
 }
 
 interface MetaProperty {
@@ -22,26 +17,10 @@ export default (meta: MetaProperty): ReactNode => {
         { hid: 'title', name: 'title', content: `${meta.title} (By: Sutan Nasution.)` },
         { hid: 'description', name: 'description', content: `${meta.description} (Made by: Sutan Nasution.)` },
         { hid: 'keywords', name: 'keywords', content: meta.keywords },
-        { name: 'theme-color', content: '#847cf3' },
-        { name: 'msapplication-TileColor', content: '#847cf3' },
-        { name: 'msapplication-TileImage', content: '/favicon/ms-icon-144x144.png' },
-        { rel: 'manifest', href: '/manifest.json' },
-        { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon/favicon.ico' },
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' },
-        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' },
-        { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon/favicon-96x96.png' },
-        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' },
-        { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/favicon/android-icon-192x192.png' },
-        { rel: 'apple-touch-icon', sizes: '57x57', href: '/favicon/apple-icon-57x57.png' },
-        { rel: 'apple-touch-icon', sizes: '60x60', href: '/favicon/apple-icon-60x60.png' },
-        { rel: 'apple-touch-icon', sizes: '72x72', href: '/favicon/apple-icon-72x72.png' },
-        { rel: 'apple-touch-icon', sizes: '76x76', href: '/favicon/apple-icon-76x76.png' },
-        { rel: 'apple-touch-icon', sizes: '114x114', href: '/favicon/apple-icon-114x114.png' },
-        { rel: 'apple-touch-icon', sizes: '120x120', href: '/favicon/apple-icon-120x120.png' },
-        { rel: 'apple-touch-icon', sizes: '144x144', href: '/favicon/apple-icon-144x144.png' },
-        { rel: 'apple-touch-icon', sizes: '152x152', href: '/favicon/apple-icon-152x152.png' },
-        { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-icon-180x180.png' }
+        { hid: 'theme-color', name: 'theme-color', content: '#847cf3' },
+        { hid: 'msapplication-TileColor', name: 'msapplication-TileColor', content: '#847cf3' },
+        { hid: 'msapplication-TileImage', name: 'msapplication-TileImage', content: '/favicon/ms-icon-144x144.png' }   
     ]
-
+    
     return metaProperties.map((props, idx) => <meta key={idx} {...props} />)
 }
